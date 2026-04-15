b198a8901c516613e9ee2ceb0ef9caf5e54653d4
Hatim <hatimlucman10@gmail.com>
2026-04-15 13:34:43 +0400

    Flatten torched_combinato structure

--- torched_combinato/torched_combinato/__init__.py
+++
@@ -1,5 +0,0 @@
-from .naive_combinato import NaiveCombinato
-from .improved_combinato import ImprovedCombinato
-from .optimized_combinato import OptimizedCombinato
-
-__all__ = ['NaiveCombinato', 'ImprovedCombinato', 'OptimizedCombinato']
